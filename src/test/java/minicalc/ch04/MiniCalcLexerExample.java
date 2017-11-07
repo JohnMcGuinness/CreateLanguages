b1460dc5bf3fcaa571981c7595ea070630fd58f6
John McGuinness <j.mcguinness@icloud.com>
2017-11-07 17:08:41 +1100

    Refactored MiniCalcLexerExample

--- src/test/java/minicalc/ch04/MiniCalcLexerExample.java
+++ src/test/java/minicalc/ch04/MiniCalcLexerExample.java
@@ -9,37 +9,33 @@ import org.minicalc.MiniCalcLexer;
 
 public class MiniCalcLexerExample
 {
-	private static final String RESOURCES_FOLDER = "src/test/resources/";
+	private static final String RESOURCES_DIR = "src/test/resources/";
 
 	public static void main(String[] args) throws IOException {
 
-		final MiniCalcLexer lexer = lexerForCode(readSourceFile(RESOURCES_FOLDER + "examples/rectangle.mc"));
+		final MiniCalcLexer lexer = lexerForCode(readSourceFile(RESOURCES_DIR + "examples/rectangle.mc"));
 
 		Token token = null;
 
 		do {
 
 			token = lexer.nextToken();
-			String typeName = MiniCalcLexer.VOCABULARY.getSymbolicName(token.getType());
 
-			String text = token.getText().replace("\n", "\\n")
-										 .replace("\r", "\\r")
-										 .replace("\t", "\\t");
-
-			System.out.println(formatToken(token.getLine(),
-										   token.getStartIndex(),
-										   token.getStopIndex(),
-										   typeName,
-										   text));
+			System.out.println(formatToken(token));
 
 		} while (-1 != token.getType());
 
 	}
 
-	private static String formatToken(int line, int startIndex, int stopIndex, String typeName, String text) {
+	private static String formatToken(Token token) {
 
-		final String template = "L{0}({1}-{2}) {3} ''{4}''";
-
-		return MessageFormat.format(template, line, startIndex, stopIndex, typeName, text);
+		return MessageFormat.format("L{0}({1}-{2}) {3} ''{4}''",
+									token.getLine(),
+									token.getStartIndex(),
+									token.getStopIndex(),
+									MiniCalcLexer.VOCABULARY.getSymbolicName(token.getType()),
+									token.getText().replace("\n", "\\n")
+												   .replace("\r", "\\r")
+												   .replace("\t", "\\t"));
 	}
 }
