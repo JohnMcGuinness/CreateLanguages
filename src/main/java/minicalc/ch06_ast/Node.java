d8cd7cfd2607dc56184c4116a2a6e593989f8262
John McGuinness <j.mcguinness@icloud.com>
2017-11-09 13:31:56 +1100

    Added methods to process Nodes

--- src/main/java/minicalc/ch06_ast/Node.java
+++ src/main/java/minicalc/ch06_ast/Node.java
@@ -1,8 +1,42 @@
 package minicalc.ch06_ast;
 
-import minicalc.ch06_ast.Position;
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public interface Node {
 
     public Position position();
+
+    public default void process(Consumer<Node> operation) {
+        operation.accept(this);
+
+        for(Field field : getClass().getDeclaredFields()) {
+
+            try {
+
+                final Object value = field.get(this);
+
+                if(value instanceof Node) {
+                    ((Node) value).process(operation);
+                }
+                else if (value instanceof Collection<?>) {
+
+                    ((Collection<?>) value).forEach(
+
+                        v -> {
+                            if(v instanceof Node) {
+                                ((Node) v).process(operation);
+                            }
+                        }
+                    );
+                }
+            }
+            catch (IllegalArgumentException | IllegalAccessException ex) {
+                Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
